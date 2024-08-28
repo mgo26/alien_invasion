273542af2d9649986cb68d46cf1a183f03f51239
Max <godecmax@gmail.com>
2024-08-28 17:16:11 +0100

    Project largely finished funciton wise, will refactor and change alien image to include more rows.

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,7 +86,10 @@ class AlienInvasion:
     def _start_game(self):
         #Reset game statistics.
         self.stats.reset_stats()
-            
+        self.sb.prep_score()
+        self.sb.prep_level()
+        self.sb.prep_ships()
+        
         #Get rid of any remaining bullets and aliens.
         self.bullets.empty()
         self.aliens.empty()
@@ -149,11 +152,21 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+        
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()   
-            self.settings.increase_speed()        
+            self.settings.increase_speed()
+            
+            #Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()        
                    
                    
     def _update_aliens(self):
@@ -224,8 +237,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships left.
+            # Decrement ships left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
